6898940c1642c1d8d8723bad9c5eaaa7cd8c9ca8
J Modliszewski <jmodlis@gmail.com>
2022-03-29 11:02:38 -0400

    Update and rename render.R to QB_SingleCell_quickpass_render.R

--- QB_SingleCell_quickpass_render.R
+++ QB_SingleCell_quickpass_render.R
@@ -2,17 +2,15 @@
 require(rmarkdown)
 library(data.table)
 
-# having problem with cached Sys.time()
+# Set output file and processed seurat files with time stamp
 ts <- format(Sys.time(), '%d%b%Y_%H%M%Z')
-
-
 output_file <- paste0("QB_SingleCell_qQC_", ts, ".html")
 seurat_processed <- paste0("./../results/QB_SingleCell_qQC/seurat_", ts, ".rds")
 
 print("Output file:")
 print(output_file)
 
-rmarkdown::render(input="QB_SingleCell_quick_pass_v2.Rmd",
+rmarkdown::render(input="QB_SingleCell_quickpass.Rmd",
                   output_file = output_file,
                   output_dir = "./../results/QB_SingleCell_qQC/",
                   envir=new.env(),
